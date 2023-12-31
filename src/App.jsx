546990be641b8f9cfcb9ff8fcfff23a2eba6665d
Albert <irgialbert@gmail.com>
2024-01-01 00:28:03 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
-import React, { useState } from "react";
-import axios from "axios";
-
 import Hero from './components/Hero'
 import Demo from './components/Demo'
-import Footer from './components/Footer'
 
 import './App.css'
 
 const App = () => {
-  const [buildStatus, setBuildStatus] = useState("");
-  const buildHookUrl = "https://api.netlify.com/build_hooks/654c4d35d9a9611fb06ff1a8"; // Replace with your actual Build Hook URL
-
-  const triggerBuild = async () => {
-    try {
-      const response = await axios.post(buildHookUrl);
-      if (response.status === 200) {
-        setBuildStatus("Build triggered successfully.");
-      }
-    } catch (error) {
-      setBuildStatus("Error triggering build.");
-      console.error("Error triggering build:", error);
-    }
-  };
   return (
     <main>
       <div className='main'>
@@ -30,12 +12,6 @@ const App = () => {
 
       <div className='app'>
         <Hero />
-        <Demo />
-        <div>
-          <button onClick={triggerBuild}>Trigger Build</button>
-          <p>{buildStatus}</p>
-        </div>
-        <Footer />
       </div>
     </main>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,201 @@
-import React from 'react'
-
-import { logo } from '../assets'
-
 const Hero = () => {
     return (
         <header className='w-full flex justify-center items-center flex-col'>
             <nav className='flex justify-between items-center w-full mb-10 pt-3 mt-5'>
-                <img src={logo} alt="logo" className='w-60 object-contain' />
+                <h1 className='head_text_1'>Mindful<span className='orange_gradient'>Youth</span></h1>
 
-                <button
-                    type="button"
-                    onClick={() => window.open('https://albertirgi.com/')}
-                    className='black_btn font-poppins mt-1'>
-                    {'</>'} albertirgi {'</>'}
-                </button>
             </nav>
 
-            <h1 className='head_text'>
-                Summar<span className='orange_gradient'>AI</span>ze Article with <br className='max-md:hidden' />
-                <span className='orange_gradient'>OpenAI GPT-4</span>
+            <h1 className='head_text mb-10'>
+                Why are <span className='orange_gradient'>Mental Health</span> issues
+                <br className='max-md:hidden' />
+                rising among <span className='orange_gradient'>young people?</span>
             </h1>
 
-            <h2 className='desc'>
-                Simplify your reading with SummarAIze, an open-source article
-                summarizer that transforms lengthy articles into clear and
-                concise summaries
+
+            <h2 className='desc_1'>
+                Every month since December 2022, we at Youth Talks publish a four-article series about a key socio-political topic, intending to trigger discussions with our readers. This month, we explore the topic of mental health, its importance and its evolution across time, with a focus on young people and global initiatives helping others take care of themselves. 
+            </h2>
+
+            <br />
+
+            <h2 className="desc_2">
+                Mental health has been increasingly discussed by the media since the COVID-19 crisis, as related deaths and hospitalizations, lockdowns and social restrictions, deeply affected it. Among other alerting numbers, global rates of depression and anxiety rose by more than 25% in 2020, with 20 to 24-year-olds enduring the largest leap of all.
+            <br />
+            <br />
+                This article’s objective is to provide you with basic knowledge about mental health, touch upon the multiple reasons why young people’s mental health was more affected by recent events than other categories of the population, and some ways to cope.
+            </h2>
+
+            <br />
+
+            <h1 className="text-3xl">
+                📚 <span className="orange_gradient">What is mental health?</span>
+            </h1>
+            <br />
+
+
+            <h2 className="desc_2">
+            According to the World Health Organization (WHO), “mental health is a state of well-being that enables people to cope with the stresses of life, realize their abilities, learn well and work well, and contribute to their community.” It is crucial because good mental health allows you to: 
+            <br />
+            <br />
+
+            <li className="ml-10">Make decisions for yourself (personal development),</li>
+            <li className="ml-10">Make friends and nourish your social life (community development),</li>
+            <li className="ml-10">Work and make a living (socio-economic development).</li>
+            <br />
+            It is as important as your body health: if you were a car, mental health would be your fuel, the energy that makes you capable of going through stuff without breaking down even when your life gets bumpy. For example in the United States, people with depression have a 40% higher risk of developing cardiovascular and metabolic diseases than the general population.
+            </h2>
+
+            <br />
+            <h1 className="text-3xl orange_gradient">
+            What are the different kinds of mental health issues?
+            </h1>
+            <br />
+
+            <h2 className="desc_2">
+            There is a wide range of mental health and the WHO defined two categories of issues based on when they emerge, how long they last, and how much they impede a person’s development:
+            <br /><br />
+            <li className="ml-10">Reactive psychological distress, caused by an event such as grief, difficulties at school, family issues. The most common symptoms are of depressive and anxious nature, often temporary and do not require specialized care. It is a “normal adaptive response” that must be taken care of so that it does not persist and worsen into an actual depression or disabling anxiety disorder.</li>
+            <li className="ml-10">Psychiatric disorders of varying duration, more or less severe and disabling, which are diagnosed according to precise criteria and taken care of thanks to targeted therapeutic actions. Such issues include personality disorders like bipolarity and require medical care.</li>
+            </h2>
+
+            <br />
+            <h1 className="text-3xl orange_gradient">
+            What can impact someone’s mental health?
+            </h1>
+            <br />
+
+            <h2 className="desc_2">
+            While each person has their life history and sensitivities, three types of determinants can affect mental health: individual, social, and structural.
+<br />
+<br />
+<li className="ml-10">At the individual level, we each have our emotional skills or can be addicted to substances, which makes us more or less vulnerable to mental health problems.</li>
+<li className="ml-10">At the social level, unfavorable circumstances like poverty, violence, and economic inequalities can increase the risks of experiencing mental health issues. On the upside, positive social interactions, a good level of education, and a safe environment can reduce those risks. </li>
+<li className="ml-10">And finally, at the structural level, events such as economic crises, pandemics, humanitarian emergencies, and environmental disasters can affect our mental health. </li>
+<br />
+Moreover, it has been found that the more unequal and polarized a country’s population, the higher the prevalence of schizophrenia, depression, anxiety and substance use. In short, the poorest groups are consistently more concerned by mental health issues.
+            </h2>
+
+            <br />
+            <h1 className="text-3xl">
+            🤔 <span className="orange_gradient">And what about the youth?</span>
+            </h1>
+            <br />
+
+            <h2 className="desc_2">
+            First of all, mental health is worsening for everyone, with a general rise in depression, anxiety, and stress. The latest Gallup Report on Global Emotions conducted at a global level and with people older than 15 concluded that “in 2021, negative emotions — the aggregate of the stress, sadness, anger, worry and physical pain that people feel every day — reached a new record” after increasing for a decade.
+            </h2>
+
+            <br />
+            <h1 className="text-3xl orange_gradient">
+            And young people?
+            </h1>
+            <br />
+
+            <h2 className="desc_2">
+Moreover, the increase in the number of youths reporting feelings of distress took the biggest leap with the COVID-19 pandemic as their feelings of self-esteem and connection to others were affected by social isolation. For example in the United States, the Pew Research Center found 45% of people under 30 felt “nervous, anxious or on edge” at least “occasionally or a moderate amount of time” while only 28% of people aged 30 and older answered the same way. Overall, 32% of adults ages 18 to 29 feel psychological distress.
+            </h2>
+
+            <br />
+            <h1 className="text-3xl orange_gradient">
+            How do we explain this increase?
+            </h1>
+            <br />
+
+            <h2 className="desc_2">
+            Numerous studies conducted to assess the impact of the COVID-19 pandemic on mental health concluded that the pandemic is the source of the increase in mental disorders among youths, especially anxiety with regards to the future that awaits them.
+            <br /><br />
+Indeed, social determinants like isolation, school closures and other restrictions, and structural determinants such as the climate crisis, economic instability and rising unemployment, have particularly disrupted their daily lives, as opposed to older people who are generally less dependent on going out for socializing and leading more stable professional careers.
+            </h2>
+
+            <br />
+            <h1 className="text-3xl orange_gradient">
+            What else?
+            </h1>
+            <br />
+
+            <h2 className="desc_2">
+            Another consequence of the outbreak was a peak in the use of social media, which worsened an existing issue, the link between mental health issues and social media. For example, from 2001 to 2017, adolescent depression rates increased by 60%, with the largest increase among women.
+            </h2>
+
+            <br />
+            <h1 className="text-3xl orange_gradient text-center">
+            The more time you spend watching screens,<br />the more depressed you can get.
+            </h1>
+            <br />
+
+            <h2 className="desc_2">
+            Yes. And the rise of social media also gave birth to “doom scrolling”, a word that emerged in 2018, which means “endlessly consume content online”. Even though it doesn’t create anxieties, it can aggravate existing ones for people prone to anxiety as they try to reassure themselves and control the news by seeking more of them. The problem is the way our brains work, they treat negative information as a threat and respond by producing cortisol, the stress hormone that amplifies anxiety.
+            </h2>
+
+            <br />
+            <h1 className="text-3xl orange_gradient">
+            What about social media?
+            </h1>
+            <br />
+
+            <h2 className="desc_2">
+            Depression, lower self-esteem, appearance anxiety, body dissatisfaction, eating disorders, and even suicidal thoughts have been linked to the use of social media, especially among young women on Instagram. The problem: when users compare themselves to others posting unrealistic pictures of themselves, it triggers feelings of self-depreciation, which can spiral into severe mental health issues.
+            </h2>
+
+            <br />
+            <h1 className="text-3xl">
+            👩‍⚕️ <span className="orange_gradient">How can I take care of my mental health?
+</span>
+            </h1>
+            <br />
+            <h1 className="text-3xl orange_gradient">
+            Should I start with less screen time?
+            </h1>
+
+            <br />
+            <h2 className="desc_2">
+            It’s an excellent first step. Everyone is vulnerable to doom scrolling, but it is possible to reduce risks of anxiety with a more thoughtful use of social networks, for example by choosing what we look at: media that broadcast good news and make us smile, credible influencers who look like us, cultural content that inspires us… And if you want to go further than that, you can mimic the young American movement made up of high schoolers abandoning their smartphones for flip phones, the Luddites.
+            </h2>
+
+            <br />
+            <h1 className="text-3xl orange_gradient">
+            What else can I do?
+            </h1>
+            <br />
+
+            <h2 className="desc_2">
+            On a daily basis, you can:
+            <br />
+<br />
+<li className="ml-10">Exercise: it releases endorphins, which reduce anxiety and stress, to make you feel good.</li>
+<li className="ml-10">Sleep 7-9 hours per night: it’s necessary to reset your brain and body, to better process emotions and memory.</li>
+<li className="ml-10">Eat well, or at least whatever makes you feel great.</li>
+<li className="ml-10">Practice breathing exercises: take deep breaths to signal your brain to calm down and slow your heart rate.</li>
+<li className="ml-10">Meditate: it can teach your brain to dissociate from your thoughts and to better manage your emotions.</li>
+            </h2>
+
+            <br />
+            <h1 className="text-3xl orange_gradient">
+            What about therapy?
+            </h1>
+            <br />
+
+            <h2 className="desc_2">
+            It can be for everyone, you don’t necessarily have to be diagnosed with a mental illness to benefit from it. There are five main types of therapy: Cognitive behavioral therapy, which focuses on thought patterns to “twitch” them in a positive way, psychodynamic therapy that relies on Freudian theories and memories, acceptance and commitment therapy comes with a focus on feelings, schema therapy helps people identify behavioral or cognitive patterns, and emotion-focused therapy helps with relationship issues.
+<br />
+<br />
+Depending on how you are feeling and your needs, there are a lot of different kinds of therapies, but a good place to start is with your usual practitioner, who can guide you with all the right info.
+            </h2>
+
+            <h2 className="desc_1">
+            We will get into more detail about different types of issues and treatments in an article that will be published in a couple of weeks, stay tuned!
+            </h2>
+
+            <br />
+            <h1 className="text-3xl">
+            🧠 <span className="orange_gradient">Recap
+</span>
+            </h1>
+            <br />
+
+            <h2 className="desc_2 mb-20">
+            A mental health that is taken care of allows you to face life events, while keeping psychological disorders at bay. Its main principle is to better access and manage emotions. Disorders are common things among the general population and have been on the rise for a decade, but young people have especially been affected by them since the COVID-19 crisis heavily impacted their lives. Thankfully, there are as many support methods as there are life experiences to help out everyone take care of their mental health.
             </h2>
         </header>
     )
